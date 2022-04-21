b0c941aae3cfdfade731598ea4b5b48ad531940a
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-21 11:29:31 -0500

    delete options in cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: true}));
 app.use("/api", function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE');
     next()
 });
 
